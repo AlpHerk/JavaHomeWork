8ee262e2830bfacf183fa3d31e56d7ff249a68b4
昂! 无白 Herk <58321084+AlpHerk@users.noreply.github.com>
2022-03-29 20:58:11 +0800

    first commit

--- 1.2.Java语法基础/CalculateObject .java
+++ 1.2.Java语法基础/CalculateObject .java
@@ -1,13 +1,16 @@
 import java.io.*;
 import java.util.Scanner;
 
-public class CalculateCircu {
+public class CalculateObject {
     public static void main(String[] args) {
         q3Circle(); // 编程题-3
         q5Sphere(); // 编程题-5
     }
-    
+
     private static void q3Circle() {
+        /**
+         * 编程题-3 圆的半径为3m, 求其周长和面积
+         */
         double r = 3.4;
         double perimeter = 2 * Math.PI * r;
         double area = Math.pow(Math.PI, 2);
@@ -17,6 +20,9 @@ public class CalculateCircu {
     }
 
     private static void q5Sphere() {
+        /** 
+         * 编程题-5，球圆球的体积
+        */
         double r1 = 10;
         double r2 = Math.random() * 10;
 
@@ -26,16 +32,16 @@ public class CalculateCircu {
         double r3 = scan.nextDouble();
         scan.close();
 
-        double v1 = CalcVolume(r1);
-        double v2 = CalcVolume(r2);
-        double v3 = CalcVolume(r3);
+        double v1 = calcVolume(r1);
+        double v2 = calcVolume(r2);
+        double v3 = calcVolume(r3);
 
         System.out.println("直接赋值r为10的球的体积：" + String.format("%.4f", v1) + "立方米");
         System.out.println("随机生成r值的球的体积：" + String.format("%.4f", v2) + "立方米");
         System.out.println("键盘输入r值的球的体积：" + String.format("%.4f", v3) + "立方米");
     }
 
-    public static double CalcVolume(double r) {
+    public static double calcVolume(double r) {
         return 4.0 / 3 * Math.PI * Math.pow(r, 3);
     }
 }
